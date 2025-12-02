151d0fd9a2a6975ef9b0341c7eb361283a1d5740
十二. <2795983512@qq.com>
2025-12-02 17:14:19 +0800

    Update 二叉树：前序+中序推后序.cpp

--- ACMICPC/二叉树：前序+中序推后序.cpp
+++ ACMICPC/二叉树：前序+中序推后序.cpp
@@ -2,27 +2,24 @@
 using namespace std;
 string inord,preord;
 int posi[256];//每个字符在中序中的位置（快速查找）
-
-
 void bulid_post(int inL,int inR,int preL,int preR)
 {
     if(inL>inR)
     return ;
-    char root = preord[preL];//根在前序开头
-    int k = posi[root];//根在中序的位置
-    int leftSize = k - inL;//左子树节点数
+    char root = preord[preL];//先找出root：先序的头就是
+    int k = posi[root];//再找出root在中序中的位置，用于后面切割分别递归两个子树
+    int leftsize = k - inL;//左子树的长度，用于递归传参
 
-    bulid_post(inL,k-1,preL+1,preL+leftSize);//递归处理左子树
-    bulid_post(k+1,inR,preL+leftSize+1,preR);//递归处理右子树
+    bulid_post(inL,k-1,preL+1,preL+leftsize);//前两个是中序遍历左子树的区间范围，后两个是先序遍历左子树的区间范围
+    bulid_post(k+1,inR,preL+leftsize+1,preR);//前两个是中序遍历右子树的区间范围，后两个是先序遍历右子树的区间范围
+    
     cout<<root;
+    
 }
-
-
-
 int main()
 {
     cin>>inord>>preord;
-    for(int i = 0;i<inord.size();i++)
+    for(int i=1;i<=inord.size();i++)
     {
         posi[inord[i]] = i;
     }
@@ -30,5 +27,7 @@ int main()
     int n = inord.size();
     bulid_post(0,n-1,0,n-1);
     cout<<endl;
+
+
     return 0;
 }
